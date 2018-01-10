0ab9ee6f09f8c8a4a52777b809b4392bce910cbf
jaripog <gopirajms1995@gmail.com>
2018-01-10 23:49:25 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -17,11 +17,7 @@
 
 
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <title>Rooster Tax</title>
